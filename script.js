3e0af214d69f78e70649db1f592faf984d238e61
natas7509 <42814959+natas7509@users.noreply.github.com>
2020-06-15 16:36:21 -0400

    resigned UI

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function setMovieData(movieIndex, moviePrice) {
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
-  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
 
   localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
@@ -48,14 +48,14 @@ function populateUI() {
 }
 
 // Movie select event
-movieSelect.addEventListener('change', e => {
+movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
   setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
 // Seat click event
-container.addEventListener('click', e => {
+container.addEventListener('click', (e) => {
   if (
     e.target.classList.contains('seat') &&
     !e.target.classList.contains('occupied')
